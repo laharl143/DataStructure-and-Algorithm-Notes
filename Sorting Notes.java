01627dafcd498b533942670604c5e16d999e9600
unknown <erskine.duenas@gmail.com>
2022-11-06 14:35:06 +0800

    update

--- Sorting Notes.java
+++ Sorting Notes.java
@@ -1,14 +1,31 @@
+//https://visualgo.net/en
+//own words 
+
 /*
-#Basics Sorts {
-    Bubble Sort:
-    Selection Sort:
-    Insertion Sort:
+#Basics Sorts { //Average Time complexity: O(n^2)
+    Bubble Sort: 
+        picks the largest number then put it in the last part of the array
+    Selection Sort: 
+        picks the smallest number then put it in the first part of the array
+    Insertion Sort: 
 }
 
-#Advance Sorts {
-    Merge Sort
-    Quick Sort
-    Radix Sort
+#Advance Sorts { //Average Time complexity: O(n log(n))
+    Merge Sort:
+    Quick Sort:
+        this type of sort will pick index 0
+        put all the number less than index 0 in the left side
+        put all the number greater than index 0 in right side
+        then cement index 0 in place
+        then iterate till done
+    Radix Sort:
+        from 0-9, numbers are group based on the ones digit
+        then tens digit
+        then hundreds digit
+        then thousands digit
+        until no more segregation is neccessary  
+        
+
 
 }
 
